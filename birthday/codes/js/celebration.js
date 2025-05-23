c8fca29bcad3566a9ad1b80a06165474b07977ec
DeShyam01 <shyamsundargayen345@gmail.com>
2025-05-23 13:14:23 +0530

    Delete birthday directory

--- birthday/codes/js/celebration.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const bgMusic = document.getElementById("firework-sound");
-
-  if (bgMusic) {
-    bgMusic.play().catch(err => {
-      console.warn("Autoplay failed. User interaction may be required.", err);
-    });
-  }
-});
