611ed21c8640904556ffd1cecbcff90b95de1c31
DeShyam01 <shyamsundargayen345@gmail.com>
2025-05-24 02:05:35 +0530

    Delete docs directory

--- docs/codes/js/bday_sound.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const bgMusic = document.getElementById("happybirthday-sound");
-
-  if (bgMusic) {
-    bgMusic.play().catch((err) => {
-      console.warn("Autoplay failed. User interaction may be required.", err);
-    });
-  }
-});
